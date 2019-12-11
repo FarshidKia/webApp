f0e2312431d7476544ff331d251d5d22ae3a89cb
FarshidKia <ninafarshidkia@gmail.com>
2019-12-11 22:36:33 +0300

    bussinessLayer,DataAccessLayer,DataEntities katmanları oluşturuldu. Entity Framework dahil edildi. Code First ile tablolar oluşturuldu.

---
+++ webApp.DataEntities/Article.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webApp.DataEntities
+{
+    [Table("Article")]
+    class Article
+    {
+        [Key,DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required, StringLength(50)]
+        public string Author{ get; set; }
+        [Required, StringLength(50)]
+        public string CategoryName { get; set; }
+        [Required, StringLength(50)]
+        public string ArticleName { get; set; }
+        [Required, StringLength(50)]
+
+        public string Title { get; set; }
+        [Required, StringLength(50)]
+        public string ImageUrl{ get; set; }
+        [Required]
+        public string AuthorAbout { get; set; }
+    }
+}
---
+++ webApp.DataEntities/Author.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace webApp.DataEntities
+{
+    [Table("Author")]
+    class Author
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required,StringLength(50)]
+        public string NameSurname { get; set; }
+        [Required, StringLength(250)]
+
+        public string AuthorAbout { get; set; }
+        [Required, StringLength(250)]
+
+        public string Image { get; set; }
+        [Required, StringLength(50)]
+        public string Email { get; set; }
+        [Required, StringLength(50)]
+        public string FacebookAddress { get; set; }
+        [Required, StringLength(50)]
+        public string TwitterAddress { get; set; }
+        [Required, StringLength(50)]
+        public string InstagramAddress { get; set; }
+        
+
+
+
+
+
+
+
+    }
+}
